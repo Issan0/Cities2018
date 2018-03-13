199ab53953a880041c22e1b776b38d3af80039fb
Student <Student@193.107.34.218>
2018-03-13 10:57:41 +0100

    lalalalala1

---
+++ Cities2018/src/cities2018/Countries.java
@@ -0,0 +1,19 @@
+
+package cities2018;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Countries {
+    private Map<String,String> countries = new HashMap();
+    
+    public Countries(){
+        countries.put("Poland", "Warsaw");
+         countries.put("Germany", "Berlin");
+          countries.put("Russia", "Moscow");
+           countries.put("Great Britan", "London");
+            countries.put("France", "Paris");
+            
+    }
+}
