89ef2dc94192967654b1b15e691267f980278f08
Student <Student@193.107.34.218>
2018-03-13 11:32:08 +0100

    miasta

--- Cities2018/src/cities2018/Cities2018.java
+++ Cities2018/src/cities2018/Cities2018.java
@@ -14,8 +14,6 @@ public class Cities2018 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+   
     
 }
--- Cities2018/src/cities2018/Countries.java
+++ Cities2018/src/cities2018/Countries.java
@@ -1,8 +1,10 @@
 
 package cities2018;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeSet;
 
 
 public class Countries {
@@ -14,6 +16,40 @@ public class Countries {
           countries.put("Russia", "Moscow");
            countries.put("Great Britan", "London");
             countries.put("France", "Paris");
+             countries.put("Spain", "Madrid");
             
     }
+    @Override
+    public String toString(){
+        return countries.toString();
+    }
+    public static void main(String[] args){
+        Countries c = new Countries();
+        System.out.println(c);
+        System.out.println(c.capitals());
+        System.out.println(c.countries());
+        System.out.println(c.sortedCapitals());
+        System.out.println(c.sortedCountries());
+    }
+
+public Collection<String> capitals ()
+{
+return countries.values();
+}
+public Collection<String> countries()
+{
+return countries.keySet();
+}
+public Collection<String> sortedCapitals()
+{
+    TreeSet set = new TreeSet();
+    set.addAll(countries.values());
+    return set;
+}
+public Collection<String> sortedCountries()
+{
+    TreeSet set = new TreeSet();
+    set.addAll(countries.keySet());
+    return set;
+}
 }
